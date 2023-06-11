650ffce86340f060c340fa494263bd8cdee353c8
Breno Ferreira Lima <brenoferreiralima@gmail.com>
2023-06-10 21:12:04 -0300

    Update aur.chaotic.cx.sh

--- aur.chaotic.cx.sh
+++ aur.chaotic.cx.sh
@@ -1,11 +1,4 @@
 ### sudo su -
 
 #!/bin/bash
-echo 'adding chaotic repo...' \
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com \
-&& pacman-key --lsign-key FBA220DFC880C036 \
-&& pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' \
-&& echo '' >> /etc/pacman.conf \
-&& echo '[chaotic-aur]' >> /etc/pacman.conf \
-&& echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf \
-echo 'chaotic repo added sucessfully!'
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com & pacman-key --lsign-key FBA220DFC880C036 & pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' -y & echo '' >> /etc/pacman.conf & echo '[chaotic-aur]' >> /etc/pacman.conf & echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
