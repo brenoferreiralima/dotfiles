21cd155a6dad6523dc96b3188e61be8f3f54fd3e
Breno Lima <brenoferreiralima@gmail.com>
2023-06-16 12:52:21 -0300

    Create fix-protonup-qt-desktop.sh

---
+++ scripts/fix-protonup-qt-desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Specify the path to the text file
+file_path="/usr/share/applications/net.davidotek.pupgui2.desktop"
+
+# Specify the line you want to replace and the new line
+old_line="Exec=net.davidotek.pupgui2"
+new_line="Exec=env QT_QPA_PLATFORM=xcb net.davidotek.pupgui2"
+
+# Use sed command to replace the line in the file
+sed -i "s/$old_line/$new_line/g" "$file_path"
