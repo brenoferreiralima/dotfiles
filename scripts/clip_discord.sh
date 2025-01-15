70b20b09bebda865dd06ebd0bf58de155278cca5
Breno Lima <brenoferreiralima@gmail.com>
2025-01-15 08:30:19 -0300

    add collor and fixes to messages

--- scripts/clip_discord.sh
+++ scripts/clip_discord.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# Re-encode a video file on a smaller size to share on discord
+
+
+# ANSI collor variables (BOLD)
+NCollor='\033[0m'     # No Collor
+BRed='\033[1;31m'     # Red
+BGreen='\033[1;32m'   # Green
+BYellow='\033[1;33m'  # Yellow
 
 # Check if the correct number of arguments is supplied
 if [ "$#" -ne 1 ]; then
-  echo "Error: Please provide video filename." >&2
+  echo -e "${BRed} Error: Please provide video filename. ${NCollor}" >&2
   exit 1
 fi
 
@@ -12,4 +20,4 @@ input_file="$1"
 # Trim the video using FFmpeg
 ffmpeg -i "$input_file" -crf 28 -preset slow -c:a copy "discord_$input_file"
 
-echo "Video converted and saved as discord_$input_file successfully!"
+echo -e "${BGreen} Video encoded and saved as discord_$input_file successfully! ${NCollor}"
--- scripts/vodtrimmer.sh
+++ scripts/vodtrimmer.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# Trim a video based on a start/end time
+
+
+# ANSI collor variables (BOLD)
+NCollor='\033[0m'     # No Collor
+BRed='\033[1;31m'     # Red
+BGreen='\033[1;32m'   # Green
+BYellow='\033[1;33m'  # Yellow
 
 # Check if the correct number of arguments is supplied
 if [ "$#" -ne 4 ]; then
-  echo "Error: Please provide video filename, start time, end time and output filename." >&2
+  echo -e "${BRed} Error: Please provide video filename, start time, end time and output filename. ${NCollor}" >&2
   exit 1
 fi
 
@@ -15,4 +23,4 @@ output_file="$4"
 # Trim the video using FFmpeg
 ffmpeg -i "$input_file" -ss "$start_time" -to "$end_time" -c copy "$output_file"
 
-echo "Video trimmed and saved as $output_file successfully!"
+echo -e "${BGreen} Video trimmed and saved as $output_file successfully! ${NCollor}"
