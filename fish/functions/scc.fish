549474ea55c7cf9c7bdfc23dc24c23c9ed6c9359
Breno Lima <brenoferreiralima@gmail.com>
2026-04-12 19:42:03 -0300

    Update scc.fish

--- fish/functions/scc.fish
+++ fish/functions/scc.fish
@@ -1,4 +1,4 @@
-# clean the system caches with yay and pacman
+# clean the system caches with yay, pacman and flatpak
 function scc --wraps='yes | yay -Scc && flatpak uninstall --unused' --description 'alias scc=yes | yay -Scc && flatpak uninstall --unused'
     yes | yay -Scc && flatpak uninstall --unused $argv     
 end
